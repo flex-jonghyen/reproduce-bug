1b09c5cb45768af9dcfec5a903dca22336ddee7c
Kim Jong Hyen <jonghyen@flex.team>
2022-04-14 20:19:50 +0900

    chore: Add sdks

--- src/SomeComponent.tsx
+++ src/SomeComponent.tsx
@@ -2,13 +2,15 @@ import { ReactNode } from "react";
 
 type 리턴타입 = ReactNode;
 type 문제타입 = {
-    name: string;
-    value: string;
-}
+  name: string;
+  value: string;
+};
 
 export const 한글컴포넌트 = ({ name, value }: 문제타입): 리턴타입 => {
-
-    return <div>
-        {name} {value}
+  return (
+    <div>
+      {name} {value}
     </div>
-}
+  );
+};
+
