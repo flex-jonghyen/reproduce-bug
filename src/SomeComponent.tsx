8b8cd3f3cbd11fc6b4034009ea57e4f5f623ceb4
Kim Jong Hyen <jonghyen@flex.team>
2022-04-14 20:40:32 +0900

    chore: Ensure error with non ascii

--- src/SomeComponent.tsx
+++ src/SomeComponent.tsx
@@ -1,12 +1,17 @@
 import { ReactElement } from "react";
 
 type 리턴타입 = ReactElement;
-type a = {
+type ü = {
   한글변수명: string;
   value: string;
 };
 
-export const 한글컴포넌트 = ({ 한글변수명, value }: a): 리턴타입 => {
+type 논아스키 = {
+  한글변수명: string;
+  value: string;
+}
+
+export const 한글컴포넌트 = ({ 한글변수명, value }: ü): 리턴타입 => {
   return (
     <div>
       {한글변수명} {value}
